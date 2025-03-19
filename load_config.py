5e756bb537d38a25d1c7bf3271bb1249e7834641
rinatsaban <rinatsaban7@gmail.com>
2025-03-19 11:08:44 +0200

    updated configs path

--- load_config.py
+++ load_config.py
@@ -2,7 +2,9 @@ import os
 import yaml
 
 def load_config(config_name='config'):
-    config_path = os.path.join(os.getcwd(), 'configs', fr'{config_name}.yaml')
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    config_path = os.path.join(base_dir, 'configs', f'{config_name}.yaml')
+    # config_path = os.path.join(os.getcwd(), 'configs', fr'{config_name}.yaml')
     with open(config_path, 'r') as file:
         config = yaml.safe_load(file)
     return config
